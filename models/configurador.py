07c8eef6ee36b723f8a89b3ef7e37414167b6f1c
juanfcogmtnez <juanfcogmtnez@gmail.com>
2021-11-03 16:08:58 +0100

    202111031608

--- models/configurador.py
+++ models/configurador.py
@@ -5,9 +5,7 @@ from odoo import fields, models, api
 
 class Configurador(models.Model):
 	_name = "configurador"
+	_inherits = {'project.task':'plan_id'}
 	name = fields.Many2one(string="Nombre")
-	plan_id = fields.Many2one(
-		comodel_name='project.task',
-		string = 'Plan de espacios'
-	) 	
+	plan_id = fields.Many2one('project.task') 	
 	active = fields.Boolean(string="Activo",default=True)
