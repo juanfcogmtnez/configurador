56a02d325ef72c770c5fc0fa50cb81b9c2a83768
juanfcogmtnez <juanfcogmtnez@gmail.com>
2021-11-04 16:18:04 +0100

    202111041617

--- models/equipacion.py
+++ models/equipacion.py
@@ -5,10 +5,53 @@ from odoo import fields, models, api
 
 class Equipacion(models.Model):
 	_name = "equipacion"
-	_inherit = ['image.mixin']
-	name = fields.Char(string='Nombre equipo')
+	name = fields.Char(string='Equipacion')
+	name_es = fields.Char(string='Equipamiento')
+	name_fr = fields.Char(string='Équipement')
+	name_en = fields.Char(string='Equipment')
 	active = fields.Boolean(string = 'Activo',default=True)
-	codigo = fields.Char(string="codigo")
-	description = fields.Text(string='Descripción')
-	superficie = fields.Float(string='Superficie')
+	ull = fields.Char(string="ULL")
+	sub_ull = fields.Char(string="SUB ULL")
+	sub_ull_2 = fields.Char(string="SUB ULL 2")
+	item_code = fields.Char(string="item-code")
 	espacio_id = fields.Many2one(comodel_name='espacios', string = 'Proyecto')
+	descripcion_es = fields.Text(string='Descripción')
+	descripcion_fr = fields.Text(string='Description')
+	descripcion_en = fields.Text(string='Description')
+	tipo = fields.Char(string="Tipo")
+	sub_tipo = fields.Char(string="Sub-tipo")
+	tipo_arq = fields.Char(string="Tipo arquitéctonico")
+	es_datasheet_es = fields.Boolean(string = 'Ficha técnica')
+	datasheet_es = fields.Binary(string="Ficha técnica")
+	datasheet_filename_es = fields.Char(string="Nombre de archivo")
+	es_datasheet_fr = fields.Boolean(string = 'Fiche technique')
+	datasheet_fr = fields.Binary(string="Fiche technique")
+	datasheet_filename_fr = fields.Char(string="Nom du fichier")
+	es_datasheet_en = fields.Boolean(string = 'Datasheet')
+	datasheet_en = fields.Binary(string="Datasheet")
+	datasheet_filename_en = fields.Char(string="File name")
+	car_alto = fields.Float(string="Alto(cm)")
+	car_ancho = fields.Float(string="Ancho(cm)")
+	car_largo = fields.Float(string="Largo(cm)")
+	car_peso = fields.Float(string="Peso(kg)")
+	superficie = fields.Float(string="Superficie (cm2)",compute="_sup_basic",stored=True)
+	@api.onchange('car_ancho','car_largo')
+	def _sup_basic(self):
+		cm_superficie = self.car_ancho * self.car_largo
+		self.superficie = cm_superficie/10000
+	ubicacion = fields.Selection(selection=[
+	('suelo','suelo'),
+	('mural','mural'),
+	('sobremesa','sobremesa'),
+	],
+	string = 'Ubicación predeterminada'
+	)
+	movilidad = fields.Boolean(string="¿Tiene el equipo una ubicación fija?", default=True)
+	fijacion = fields.Boolean(string="¿Necesita el equipo alguna fijación especial?", default=False)
+	estructura = fields.Boolean(string="¿Necesita el equipamiento de cambios constructivos en la estructura?", default=False)
+	conex_agua = fields.Boolean(string="¿Necesita el equipo conexión a circuito de agua corriente?", default=False)
+	conex_tta = fields.Boolean(string="¿Necesita el equipo conexión a circuito de agua especial?", default=False)
+	conex_elect = fields.Boolean(string="¿Necesita el equipo conexión a circuito eléctrico corriente?", default=False)
+ 	#conex_ups = fields.Boolean(string="¿Necesita el equipo conexión a circuito eléctrico de emergencia?", default=False)
+	conex_com = fields.Boolean(string="¿Necesita el equipo conexión a red de comunicaciones?", default=False)
+	conex_ups = fields.Boolean(string="¿Necesita el equipo conexión a circuito eléctrico de emergencia?", default=False)
